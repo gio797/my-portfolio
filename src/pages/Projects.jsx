ecc88d9a978581e2a47b99f59a72b531c012a39c
gio797 <119562363+gio797@users.noreply.github.com>
2023-07-08 20:01:51 +0400

    add project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,6 +3,7 @@ import img from "../images/proj1.png";
 import img2 from "../images/proj2.png";
 import img3 from "../images/proj3.png";
 import img4 from "../images/proj4.png";
+import img5 from "../images/multiform.jpg";
 import { Link } from "react-router-dom";
 
 function Projects() {
@@ -10,14 +11,14 @@ function Projects() {
     <div className="project-page-wrapper">
       <h1 className="projects-header">Projects</h1>
       <p>
-        Here we have some of my projects, wich include links to the live
+        Here we have some of my projects, which include links to the live
         version. you can also find codes for these projects on my github.
       </p>
       <div className="project-wrapper">
         <div className="project">
           <h2>Space tourism multi-page website</h2>
           <p>
-            This is a frontendmentor website challange named 'Space tourism
+            This is a frontendmentor website challenge named 'Space tourism
             multi-page website'. In this project i have used react router and
             the page is responsive.
           </p>
@@ -42,8 +43,8 @@ function Projects() {
         <div className="project">
           <h2>Dictionary web app</h2>
           <p>
-            This is a frontendmentor website challange named 'Dictionary web
-            app'. In this project i have used react. wepsite is responsive and
+            This is a frontendmentor website challenge named 'Dictionary web
+            app'. In this project i have used react. website is responsive and
             has theme change possibility.
           </p>
           <Link to={"https://superb-druid-9d3988.netlify.app"} target="blank">
@@ -64,9 +65,9 @@ function Projects() {
         <div className="project">
           <h2>REST Countries API with color theme switcher</h2>
           <p>
-            This is a frontendmentor website challange named 'REST Countries API
+            This is a frontendmentor website challenge named 'REST Countries API
             with color theme switcher'. In this project I have used react.
-            wepsite is responsive and has theme change possibility.
+            website is responsive and has theme change possibility.
           </p>
           <Link
             to={"https://gorgeous-cobbler-04f350.netlify.app"}
@@ -103,6 +104,30 @@ function Projects() {
           <Link
             className="project-link"
             to={"https://verdant-frangollo-9bebd7.netlify.app"}
+            target="blank"
+          >
+            Project link
+          </Link>
+          <hr />
+        </div>
+
+        <div className="project">
+          <h2>Multi Page Form</h2>
+          <p>
+            This is a multi page form, frontendmentor website challenge. In this
+            project I have used react. website is also responsive.
+          </p>
+          <Link
+            to={"https://gregarious-dragon-0d3381.netlify.app"}
+            target="blank"
+          >
+            <div className="project-img-wrapper">
+              <img src={img5} alt="" className="project-img" />
+            </div>
+          </Link>
+          <Link
+            className="project-link"
+            to={"https://gregarious-dragon-0d3381.netlify.app"}
             target="blank"
           >
             Project link
